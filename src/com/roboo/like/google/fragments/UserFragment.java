5237f2b33d68697309ccd04930902749fca89ec8
haikuowuya <haikuowuya@gmail.com>
2014-03-18 17:38:00 +0800

    第一次提交

--- src/com/roboo/like/google/fragments/UserFragment.java
+++ src/com/roboo/like/google/fragments/UserFragment.java
@@ -1,16 +1,19 @@
 package com.roboo.like.google.fragments;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.roboo.like.google.R;
+import com.roboo.like.google.views.CirclePageIndicator;
 import com.roboo.like.google.views.StickyGridHeadersGridView;
 
 public class UserFragment extends BaseFragment  
 {
-	 
+	 private ViewPager mViewPager;
+	 private CirclePageIndicator mIndicator;
 
 	public static UserFragment newInstance()
 	{
@@ -22,6 +25,8 @@ public class UserFragment extends BaseFragment
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
 		View view = inflater.inflate(R.layout.fragment_user, null);
+		mViewPager = (ViewPager) view.findViewById(R.id.vp_pager);
+		mIndicator = (CirclePageIndicator) view.findViewById(R.id.cpi_indicator);
 		return view;
 	}
 
